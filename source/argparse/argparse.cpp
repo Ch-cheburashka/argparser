d14e9b54ef3903250bf6cd53f1c318d00daee455
Ch-cheburashka <m9056883999@gmail.com>
2023-03-18 23:46:18 +0300

    added string_tokenizing function

--- source/argparse/argparse.cpp
+++ source/argparse/argparse.cpp
@@ -5,57 +5,50 @@
 #include <argparse/argparse.hpp>
 #include <filesystem>
 
+constexpr char* DEFAULT_STR = "default";
+
+std::map<std::string,std::string> string_tokenizing(int argc, char** argv) {
+    std::map<std::string,std::string> tokens;
+    for (auto i = 1; i < argc; i++) {
+        if (i != argc - 1 && argv[i+1][0] != '-')
+            tokens.emplace(argv[i],argv[i+1]);
+        else
+            tokens.emplace(argv[i],DEFAULT_STR);
+    }
+    return tokens;
+}
+
 std::map<std::string,typed_value> argparse::parse_args(int argc, char **argv) {
     std::map<std::string,typed_value> options_map;
-    for (auto i = 1; i < argc; i++) {
-        if (argv[i][0] == '-') {
-            if (argv[i][1] == '-') {
-                for (auto &&option: options) {
-                    if (option.long_name == argv[i]) {
-                        if (i != argc - 1 && argv[i+1][0] != '-') {
-                            if (option.default_value.m_default_value.type() == typeid(int))
-                                options_map.emplace(option.long_name.substr(2),
-                                                    value<int>().default_value(std::stoi(argv[i + 1])));
-                            else if (option.default_value.m_default_value.type() == typeid(double))
-                                options_map.emplace(option.long_name.substr(2),
-                                                    value<double>().default_value(std::stod(argv[i + 1])));
-                            else if (option.default_value.m_default_value.type() == typeid(bool))
-                                options_map.emplace(option.long_name.substr(2),
-                                                    value<bool>().default_value(std::stoi(argv[i + 1])));
-                            else if (option.default_value.m_default_value.type() == typeid(std::filesystem::path))
-                                options_map.emplace(option.long_name.substr(2),
-                                                    value<std::filesystem::path>().default_value(std::filesystem::path(argv[i + 1])));
-                        }
-                        else {
-                            options_map.emplace(option.long_name.substr(2),
-                                                option.default_value);
-                        }
-                    }
-                }
+    auto tokens = string_tokenizing(argc,argv);
+
+    for (auto i = 1; i < argc;) {
+        for (auto &&option: options) {
+            if (tokens.at(argv[i]) == DEFAULT_STR) {
+                if (option.long_name != argv[i] && option.short_name != argv[i])
+                    continue;
+                options_map.emplace(option.long_name.substr(2),
+                                    option.default_value);
+                i++;
+                break;
             }
             else {
-                for (auto &&option: options) {
-                    if (option.short_name == argv[i]) {
-                        if (i != argc - 1 && argv[i+1][0] != '-') {
-                            if (option.default_value.m_default_value.type() == typeid(int))
-                                options_map.emplace(option.long_name.substr(2),
-                                                    value<int>().default_value(std::stoi(argv[i + 1])));
-                            else if (option.default_value.m_default_value.type() == typeid(double))
-                                options_map.emplace(option.long_name.substr(2),
-                                                    value<double>().default_value(std::stod(argv[i + 1])));
-                            else if (option.default_value.m_default_value.type() == typeid(bool))
-                                options_map.emplace(option.long_name.substr(2),
-                                                    value<bool>().default_value(std::stoi(argv[i + 1])));
-                            else if (option.default_value.m_default_value.type() == typeid(std::filesystem::path))
-                                options_map.emplace(option.long_name.substr(2),
-                                                    value<std::filesystem::path>().default_value(std::filesystem::path(argv[i + 1])));
-                        }
-                        else {
-                            options_map.emplace(option.long_name.substr(2),
-                                                option.default_value);
-                        }
-                    }
-                }
+                if (option.long_name != argv[i] && option.short_name != argv[i])
+                    continue;
+                if (option.default_value.m_default_value.type() == typeid(int))
+                    options_map.emplace(option.long_name.substr(2),
+                                        value<int>().default_value(std::stoi(tokens.at(argv[i]))));
+                else if (option.default_value.m_default_value.type() == typeid(double))
+                    options_map.emplace(option.long_name.substr(2),
+                                        value<double>().default_value(std::stod(tokens.at(argv[i]))));
+                else if (option.default_value.m_default_value.type() == typeid(bool))
+                    options_map.emplace(option.long_name.substr(2),
+                                        value<bool>().default_value(std::stoi(tokens.at(argv[i]))));
+                else if (option.default_value.m_default_value.type() == typeid(std::filesystem::path))
+                    options_map.emplace(option.long_name.substr(2),
+                                        value<std::filesystem::path>().default_value(std::filesystem::path(tokens.at(argv[i]))));
+                i+=2;
+                break;
             }
         }
     }
